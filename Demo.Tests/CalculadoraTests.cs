a9f1500300b47bb9c45caac05bd9e0fa2ae6578b
Victor Alves <victor.earn@outlook.com>
2021-03-10 20:45:56 -0300

    Theory e Fact

---
+++ Demo.Tests/CalculadoraTests.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+
+namespace Demo.Tests
+{
+    public class CalculadoraTests
+    {
+        [Fact]
+        public void Calculadora_Somar_RetornarValorSoma()
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            //Act
+            var resultado = calculadora.Somar(2, 2);
+
+            // Assert
+            Assert.Equal(expected: 4, actual: resultado);
+        }
+
+        [Theory]
+        [InlineData(1, 1, 2)]
+        [InlineData(2, 2, 4)]
+        [InlineData(4, 2, 6)]
+        [InlineData(4, 3, 7)]
+        [InlineData(9, 1, 10)]
+        [InlineData(6, 3, 9)]
+        [InlineData(1, 7, 8)]
+        public void Calculadora_Somar_RetornarValoresSomaCorretos(double v1, double v2, double total)
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            //Act
+            var resultado = calculadora.Somar(v1, v2);
+
+            // Assert
+            Assert.Equal(expected: total, actual: resultado);
+        }
+    }
+}
